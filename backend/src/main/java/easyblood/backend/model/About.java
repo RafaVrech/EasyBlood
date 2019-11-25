03294d97376fb01b159f0f558d84a90c002b49fa
rafavrech <rafavrech@gmail.com>
2019-11-25 18:45:37 -0300

    Adicionando about

---
+++ backend/src/main/java/easyblood/backend/model/About.java
@@ -0,0 +1,8 @@
+package easyblood.backend.model;
+
+import lombok.Data;
+
+@Data
+public class About {
+    private String about;
+}
---
+++ backend/src/main/java/easyblood/backend/service/AboutService.java
@@ -0,0 +1,6 @@
+package easyblood.backend.service;
+
+import easyblood.backend.model.BloodCenter;
+
+public interface AboutService {
+}
